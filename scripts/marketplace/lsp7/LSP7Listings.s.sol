aaf76d2723e19c2cdeae81338d44554ed90f0aa4
Volodymyr Lykhonis <volodymyr@lykhonis.com>
2024-01-25 11:48:27 +0100

    Configure operator for listings

--- scripts/marketplace/lsp7/LSP7Listings.s.sol
+++ scripts/marketplace/lsp7/LSP7Listings.s.sol
@@ -9,6 +9,7 @@ import {
     ITransparentUpgradeableProxy
 } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {LSP7Listings} from "../../../src/marketplace/lsp7/LSP7Listings.sol";
+import {OPERATOR_ROLE} from "../../../src/marketplace/common/Module.sol";
 
 contract Deploy is Script {
     function run() external {
@@ -24,9 +25,7 @@ contract Deploy is Script {
             vm.broadcast(admin);
             proxy = address(
                 new TransparentUpgradeableProxy(
-                    address(listings),
-                    admin,
-                    abi.encodeWithSelector(LSP7Listings.initialize.selector, owner)
+                    address(listings), admin, abi.encodeWithSelector(LSP7Listings.initialize.selector, owner)
                 )
             );
             console.log(string.concat("LSP7Listings: deploy ", Strings.toHexString(address(proxy))));
@@ -34,6 +33,19 @@ contract Deploy is Script {
             vm.broadcast(admin);
             ITransparentUpgradeableProxy(proxy).upgradeTo(address(listings));
             console.log(string.concat("LSP7Listings: upgrade ", Strings.toHexString(address(proxy))));
+        }
+    }
+}
+
+contract Configure is Script {
+    function run() external {
+        address owner = vm.envAddress("OWNER_ADDRESS");
+        address operator = vm.envAddress("MARKETPLACE_OPERATOR_ADDRESS");
+        LSP7Listings listings = LSP7Listings(vm.envAddress("CONTRACT_LSP7_LISTINGS_ADDRESS"));
+
+        if (!listings.hasRole(operator, OPERATOR_ROLE)) {
+            vm.broadcast(owner);
+            listings.grantRole(operator, OPERATOR_ROLE);
         }
     }
 }
--- scripts/marketplace/lsp8/LSP8Listings.s.sol
+++ scripts/marketplace/lsp8/LSP8Listings.s.sol
@@ -9,6 +9,7 @@ import {
     ITransparentUpgradeableProxy
 } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {LSP8Listings} from "../../../src/marketplace/lsp8/LSP8Listings.sol";
+import {OPERATOR_ROLE} from "../../../src/marketplace/common/Module.sol";
 
 contract Deploy is Script {
     function run() external {
@@ -24,9 +25,7 @@ contract Deploy is Script {
             vm.broadcast(admin);
             proxy = address(
                 new TransparentUpgradeableProxy(
-                    address(listings),
-                    admin,
-                    abi.encodeWithSelector(LSP8Listings.initialize.selector, owner)
+                    address(listings), admin, abi.encodeWithSelector(LSP8Listings.initialize.selector, owner)
                 )
             );
             console.log(string.concat("LSP8Listings: deploy ", Strings.toHexString(address(proxy))));
@@ -34,6 +33,19 @@ contract Deploy is Script {
             vm.broadcast(admin);
             ITransparentUpgradeableProxy(proxy).upgradeTo(address(listings));
             console.log(string.concat("LSP8Listings: upgrade ", Strings.toHexString(address(proxy))));
+        }
+    }
+}
+
+contract Configure is Script {
+    function run() external {
+        address owner = vm.envAddress("OWNER_ADDRESS");
+        address operator = vm.envAddress("MARKETPLACE_OPERATOR_ADDRESS");
+        LSP8Listings listings = LSP8Listings(vm.envAddress("CONTRACT_LSP8_LISTINGS_ADDRESS"));
+
+        if (!listings.hasRole(operator, OPERATOR_ROLE)) {
+            vm.broadcast(owner);
+            listings.grantRole(operator, OPERATOR_ROLE);
         }
     }
 }
