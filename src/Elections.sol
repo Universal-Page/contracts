951afdfe0dc5ad97c1b4434f28a3f5139094d738
Volodymyr Lykhonis <volodymyr@lykhonis.com>
2024-04-01 19:08:14 +0200

    Base logic for nomination, election, recycling

--- src/Elections.sol
+++ src/Elections.sol
@@ -4,27 +4,30 @@ pragma solidity =0.8.22;
 import {OwnableUnset} from "@erc725/smart-contracts/contracts/custom/OwnableUnset.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import {ILSP7DigitalAsset} from "@lukso/lsp-smart-contracts/contracts/LSP7DigitalAsset/ILSP7DigitalAsset.sol";
 
 struct Nomination {
+    uint256 blockNumber;
+    address nominator;
     uint256 amount;
     bytes data;
-    bytes32[] delegations;
+    uint128 delegations;
+    uint256 delegatedAmount;
 }
 
 struct Delegation {
     address delegator;
     uint256 amount;
-    bool bound;
+    bytes32 nomination;
+    uint128 index;
 }
 
 bytes32 constant ELECTIONS_CURATOR_ROLE = keccak256("ELECTIONS_CURATOR_ROLE");
 
 contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradeable {
-    uint8 private constant _MAX_SLOTS = 5;
-    uint256 private constant _SLOT_DURATION = 3 days;
-    uint256 private constant _MIN_NOMINATION_PRICE = 0.01 ether;
-    uint256 private constant _MIN_DELEGATION_PRICE = 0.01 ether;
-    uint8 private constant _MAX_ELECTION_CYCLES = 3;
+    uint256 private constant _MINIMUM_ELECTION_BLOCKS = 5000; // ~16 hours
+    uint256 private constant _DELEGATION_THRESHOLD = 1_000; // 0.1%
+    uint256 private constant _NOMINATION_EXPIRATION_BLOCKS = 21000; // ~3 days
 
     error InvalidAmount(uint256 amount, uint256 expected);
     error AlreadyNominated(address nominator, bytes data);
@@ -33,23 +36,31 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
     error InvalidDelegation(address delegatee, address delegator, bytes data);
     error RoleNotGranted(address account, bytes32 role);
     error RoleUnchanged(address account, bytes32 role);
-    error NotOpenElection(uint256 nextElectionTime);
     error Electing();
+    error NotElecting();
+    error NominationHasDelegations(address nominator, bytes data);
+    error InvalidElectionBlock(uint256 blockNumber);
 
     event Nominated(address indexed nominator, uint256 amount, bytes data);
-    event Retracted(address indexed nominator, uint256 amount, bytes data);
     event Elected(address indexed nominator, uint256 nominatedAmount, uint256 delegatedAmount, bytes data);
-    event Undelegated(address indexed nominator, address indexed delegator, uint256 amount, bytes data);
     event Delegated(address indexed delegatee, address indexed delegator, uint256 amount, bytes data);
+    event Undelegated(address indexed nominator, address indexed delegator, uint256 amount, bytes data);
+    event Recycled(address indexed account, uint256 amount);
     event RoleGranted(address indexed account, bytes32 role);
     event RoleRevoked(address indexed account, bytes32 role);
-    event ClaimedElected(address indexed account, uint256 amount, address indexed beneficiary);
+    event NominationPriceChanged(uint256 price);
+    event DelegationPriceChanged(uint256 price);
+    event ElectionScheduled(uint256 blockNumber);
 
+    ILSP7DigitalAsset public electionToken;
+    uint256 public electionBlock;
+    uint256 public nominationPrice;
+    uint256 public delegationPrice;
+    uint256 public recycledAmount;
     mapping(bytes32 role => mapping(address account => bool granted)) private _roles;
     mapping(bytes32 nominationKey => Nomination) private _nominations;
+    mapping(bytes32 nominationKey => mapping(uint128 index => bytes32 delegationKey)) private _nominationDelegations;
     mapping(bytes32 delegationKey => Delegation) private _delegations;
-    uint256 public electionTime;
-    uint256 public electedAmount;
 
     modifier onlyRole(bytes32 role) {
         if (!hasRole(msg.sender, role)) {
@@ -59,9 +70,12 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
     }
 
     modifier whenNotElecting() {
-        if (block.timestamp >= electionTime) {
-            revert Electing();
-        }
+        _checkNotElecting();
+        _;
+    }
+
+    modifier whenElecting() {
+        _checkElecting();
         _;
     }
 
@@ -69,10 +83,30 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
         _disableInitializers();
     }
 
-    function initialize(address newOwner_) external initializer {
+    function initialize(address newOwner_, ILSP7DigitalAsset electionToken_) external initializer {
         __ReentrancyGuard_init();
         __Pausable_init();
         _setOwner(newOwner_);
+        electionToken = electionToken_;
+        electionBlock = block.number;
+        nominationPrice = 1 ether;
+        delegationPrice = 0.1 ether;
+    }
+
+    function isElecting() public view returns (bool) {
+        return block.number >= electionBlock;
+    }
+
+    function _checkElecting() private view {
+        if (!isElecting()) {
+            revert NotElecting();
+        }
+    }
+
+    function _checkNotElecting() private view {
+        if (isElecting()) {
+            revert Electing();
+        }
     }
 
     function pause() external onlyOwner {
@@ -81,6 +115,16 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function setNominationPrice(uint256 newNominationPrice) external onlyOwner {
+        nominationPrice = newNominationPrice;
+        emit NominationPriceChanged(newNominationPrice);
+    }
+
+    function setDelegationPrice(uint256 newDelegationPrice) external onlyOwner {
+        delegationPrice = newDelegationPrice;
+        emit DelegationPriceChanged(newDelegationPrice);
     }
 
     function hasRole(address account, bytes32 role) public view returns (bool) {
@@ -111,7 +155,7 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
         emit RoleRevoked(account, role);
     }
 
-    function _nominatorKey(address nominator, bytes calldata data) private pure returns (bytes32) {
+    function _nominatorKey(address nominator, bytes memory data) private pure returns (bytes32) {
         return keccak256(abi.encodePacked(nominator, data));
     }
 
@@ -119,136 +163,178 @@ contract Elections is OwnableUnset, ReentrancyGuardUpgradeable, PausableUpgradea
         return keccak256(abi.encodePacked(delegator, nomitationKey));
     }
 
-    function nominate(bytes calldata data) external payable whenNotPaused nonReentrant {
+    function isNominated(address nominator, bytes calldata data) public view returns (bool) {
+        return _nominations[_nominatorKey(nominator, data)].amount > 0;
+    }
+
+    function _checkNomination(address nominator, bytes calldata data) private view {
+        if (!isNominated(nominator, data)) {
+            revert InvalidNomination(nominator, data);
+        }
+    }
+
+    function nominate(bytes calldata data, uint256 amount)
+        external
+        payable
+        whenNotPaused
+        whenNotElecting
+        nonReentrant
+    {
         address nominator = msg.sender;
-        uint256 amount = msg.value;
-        if (amount < _MIN_NOMINATION_PRICE) {
-            revert InvalidAmount({amount: amount, expected: _MIN_NOMINATION_PRICE});
+        if (amount < nominationPrice) {
+            revert InvalidAmount({amount: amount, expected: nominationPrice});
         }
         bytes32 nominationKey = _nominatorKey(nominator, data);
         Nomination memory nomination = _nominations[nominationKey];
         if (nomination.amount > 0) {
             revert AlreadyNominated(nominator, data);
         }
-        _nominations[nominationKey] = Nomination({amount: amount, data: data, delegations: new bytes32[](0)});
+
+        _nominations[nominationKey] = Nomination({
+            blockNumber: block.number,
+            nominator: nominator,
+            amount: amount,
+            data: data,
+            delegatedAmount: 0,
+            delegations: 0
+        });
+        electionToken.transfer(nominator, address(this), amount, true, "0x");
         emit Nominated(nominator, amount, data);
     }
 
-    function retract(bytes calldata data) external whenNotPaused whenNotElecting nonReentrant {
-        address nominator = msg.sender;
-        bytes32 nominationKey = _nominatorKey(nominator, data);
-
-        Nomination memory nomination = _nominations[nominationKey];
-        if (nomination.amount == 0) {
-            revert InvalidNomination(nominator, data);
-        }
-        _nominations[nominationKey].amount = 0;
-
-        // Retract all delegations
-        for (uint256 i = 0; i < nomination.delegations.length; i++) {
-            bytes32 delegationKey = nomination.delegations[i];
-            _delegations[delegationKey].bound = false;
-            Delegation memory delegation = _delegations[delegationKey];
-            if (delegation.amount > 0) {
-                _delegations[delegationKey].amount = 0;
-                emit Undelegated(nominator, delegation.delegator, delegation.amount, data);
-                (bool undelegated,) = delegation.delegator.call{value: delegation.amount}("");
-                if (!undelegated) {
-                    revert Unpaid(delegation.delegator, delegation.amount);
-                }
-            }
-        }
-
-        emit Retracted(nominator, nomination.amount, data);
-        (bool retracted,) = nominator.call{value: nomination.amount}("");
-        if (!retracted) {
-            revert Unpaid(nominator, nomination.amount);
-        }
+    function isDelegated(address delegatee, address delegator, bytes calldata data) public view returns (bool) {
+        return _delegations[_delegationKey(delegator, _nominatorKey(delegatee, data))].amount > 0;
     }
 
-    function delegate(address delegatee, bytes calldata data) external payable whenNotPaused nonReentrant {
+    function delegate(address delegatee, bytes calldata data, uint256 amount)
+        external
+        payable
+        whenNotPaused
+        whenNotElecting
+        nonReentrant
+    {
+        _checkNomination(delegatee, data);
+        if (amount < delegationPrice) {
+            revert InvalidAmount({amount: amount, expected: delegationPrice});
+        }
+
         address delegator = msg.sender;
         bytes32 nominationKey = _nominatorKey(delegatee, data);
         bytes32 delegationKey = _delegationKey(delegator, nominationKey);
 
-        uint256 amount = msg.value;
-        if (amount < _MIN_DELEGATION_PRICE) {
-            revert InvalidAmount({amount: amount, expected: _MIN_DELEGATION_PRICE});
-        }
-
         Nomination storage nomination = _nominations[nominationKey];
-        if (nomination.amount == 0) {
-            revert InvalidNomination(delegatee, data);
+        uint256 amountThreshold = nomination.delegatedAmount * _DELEGATION_THRESHOLD / 100_000;
+        if (amount < amountThreshold) {
+            revert InvalidAmount({amount: amount, expected: amountThreshold});
         }
 
         Delegation storage delegation = _delegations[delegationKey];
-        delegation.delegator = delegator;
         delegation.amount += amount;
-        if (!delegation.bound) {
-            delegation.bound = true;
-            nomination.delegations.push(delegationKey);
+        nomination.delegatedAmount += amount;
+
+        if (delegation.nomination != nominationKey) {
+            delegation.nomination = nominationKey;
+            delegation.delegator = delegator;
+            delegation.index = nomination.delegations;
+            _nominationDelegations[nominationKey][nomination.delegations] = delegationKey;
+            nomination.delegations += 1;
         }
+
+        electionToken.transfer(delegator, address(this), amount, true, "0x");
         emit Delegated(delegatee, delegator, amount, data);
     }
 
     function undelegate(address delegatee, bytes calldata data) external whenNotPaused whenNotElecting nonReentrant {
+        _checkNomination(delegatee, data);
+
         address delegator = msg.sender;
         bytes32 nominationKey = _nominatorKey(delegatee, data);
         bytes32 delegationKey = _delegationKey(delegator, nominationKey);
+
         Delegation memory delegation = _delegations[delegationKey];
         if (delegation.amount == 0) {
             revert InvalidDelegation(delegatee, delegator, data);
         }
-        _delegations[delegationKey].amount = 0;
-        emit Undelegated(delegatee, delegator, delegation.amount, data);
-        (bool undelegated,) = delegator.call{value: delegation.amount}("");
-        if (!undelegated) {
-            revert Unpaid(delegator, delegation.amount);
+        delete _delegations[delegationKey];
+        delete _nominationDelegations[nominationKey][delegation.index];
+
+        Nomination storage nomination = _nominations[nominationKey];
+        nomination.delegatedAmount -= delegation.amount;
+        nomination.delegations -= 1;
+
+        electionToken.transfer(address(this), delegation.delegator, delegation.amount, true, "0x");
+        emit Undelegated(delegatee, delegation.delegator, delegation.amount, data);
+    }
+
+    function scheduleElection(uint256 newElectionBlock)
+        external
+        whenNotPaused
+        whenElecting
+        nonReentrant
+        onlyRole(ELECTIONS_CURATOR_ROLE)
+    {
+        if (block.number + _MINIMUM_ELECTION_BLOCKS <= newElectionBlock) {
+            revert InvalidElectionBlock(newElectionBlock);
+        }
+        electionBlock = newElectionBlock;
+        emit ElectionScheduled(newElectionBlock);
+    }
+
+    function recycle(address nominator, bytes calldata data)
+        external
+        whenNotPaused
+        whenElecting
+        nonReentrant
+        onlyRole(ELECTIONS_CURATOR_ROLE)
+    {
+        _checkNomination(nominator, data);
+        bytes32 nominationKey = _nominatorKey(nominator, data);
+        Nomination memory nomination = _nominations[nominationKey];
+        if (nomination.blockNumber + _NOMINATION_EXPIRATION_BLOCKS < block.number) {
+            revert InvalidNomination(nominator, data);
+        }
+        delete _nominations[nominationKey];
+        emit Recycled(nominator, nomination.amount);
+        recycledAmount += nomination.amount;
+
+        for (uint128 i = 0; i < nomination.delegations; i++) {
+            bytes32 delegationKey = _nominationDelegations[nominationKey][i];
+            Delegation memory delegation = _delegations[delegationKey];
+            delete _delegations[delegationKey];
+            delete _nominationDelegations[nominationKey][i];
+
+            // try to undelegate, if denied, recycle the delegation
+            try electionToken.transfer(address(this), delegation.delegator, delegation.amount, true, "0x") {
+                emit Undelegated(nominator, delegation.delegator, delegation.amount, data);
+            } catch {
+                recycledAmount += delegation.amount;
+                emit Recycled(delegation.delegator, delegation.amount);
+            }
         }
     }
 
     function elect(address nominator, bytes calldata data)
         external
         whenNotPaused
+        whenElecting
         nonReentrant
         onlyRole(ELECTIONS_CURATOR_ROLE)
     {
-        uint256 timestamp = block.timestamp;
-        if (timestamp < electionTime) {
-            revert NotOpenElection(electionTime);
-        }
-        electionTime = timestamp + _SLOT_DURATION;
-
+        _checkNomination(nominator, data);
         bytes32 nominationKey = _nominatorKey(nominator, data);
-        Nomination storage nomination = _nominations[nominationKey];
-        if (nomination.amount == 0) {
-            revert InvalidNomination(nominator, data);
-        }
-        uint256 nominatedAmount = nomination.amount;
-        nomination.amount = 0;
+        Nomination memory nomination = _nominations[nominationKey];
+        delete _nominations[nominationKey];
+        emit Recycled(nominator, nomination.amount);
 
-        uint256 delegatedAmount = 0;
-        for (uint256 i = 0; i < nomination.delegations.length; i++) {
-            bytes32 delegationKey = nomination.delegations[i];
-            Delegation storage delegation = _delegations[delegationKey];
-            delegatedAmount += delegation.amount;
-            delegation.amount = 0;
-            delegation.bound = false;
+        for (uint128 i = 0; i < nomination.delegations; i++) {
+            bytes32 delegationKey = _nominationDelegations[nominationKey][i];
+            Delegation memory delegation = _delegations[delegationKey];
+            delete _delegations[delegationKey];
+            delete _nominationDelegations[nominationKey][i];
+            emit Recycled(delegation.delegator, delegation.amount);
         }
 
-        electedAmount += nominatedAmount + delegatedAmount;
-        emit Elected(nominator, nominatedAmount, delegatedAmount, data);
-    }
-
-    function claimElected(uint256 amount, address beneficiary) external onlyOwner {
-        if (amount > electedAmount) {
-            revert InvalidAmount({amount: amount, expected: electedAmount});
-        }
-        electedAmount -= amount;
-        emit ClaimedElected(msg.sender, amount, beneficiary);
-        (bool claimed,) = beneficiary.call{value: amount}("");
-        if (!claimed) {
-            revert Unpaid(beneficiary, amount);
-        }
+        recycledAmount += nomination.amount + nomination.delegatedAmount;
+        emit Elected(nominator, nomination.amount, nomination.delegatedAmount, data);
     }
 }
